8bcef88e38da8783200d99afa0a359bbc8dbea90
islamul2512 <islamul2512@gmail.com>
2023-12-21 15:31:53 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3: Create an object of class RWOnly
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Attempt to directly access the private variable (error expected)
+        // Uncommenting the below line will cause a compile-time error
+        // System.out.println(obj.name);
+
+        // Task 6: Set a value to 'name' using the setter and access it using the getter
+        obj.setName("John");
+        System.out.println("Name: " + obj.getName());
+    }
 }
